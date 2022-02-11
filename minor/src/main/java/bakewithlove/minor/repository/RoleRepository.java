b40c8007201a92e64d5e52b3708c436eb48faa54
Akansh-Verma <58855711+Akansh-Verma@users.noreply.github.com>
2022-02-11 23:58:34 +0530

    Delete minor directory

--- minor/src/main/java/bakewithlove/minor/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package bakewithlove.minor.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import bakewithlove.minor.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role,Integer>{
-    
-}
